8c138975a95884586fcb735683e7066578e0f22b
Valters Vingolds <valters@vingolds.ch>
2019-03-25 09:42:02 +0100

    javadoc: fix up warning

--- src/main/java/com/j256/cloudwatchlogbackappender/MiscUtils.java
+++ src/main/java/com/j256/cloudwatchlogbackappender/MiscUtils.java
@@ -2,15 +2,17 @@ package com.j256.cloudwatchlogbackappender;
 
 /**
  * Set of common utility methods copied from the Net.
- * 
+ *
  * @author graywatson
  */
 public class MiscUtils {
 
 	/**
 	 * Return true if the string is null, empty, or all whitespace, otherwise false.
+	 * @param cs to check
+	 * @return false is string has content
 	 */
-	public static boolean isBlank(CharSequence cs) {
+	public static boolean isBlank(final CharSequence cs) {
 		if (cs == null || cs.length() == 0) {
 			return true;
 		}
